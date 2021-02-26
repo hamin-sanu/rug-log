f760ad03b9af5122d907f3056205d4121220dd20
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-02-26 18:54:10 +0900

    seed_fuでカテゴリテーブル作成
    ・gemを入れたり等

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_031750) do
+ActiveRecord::Schema.define(version: 2021_02_26_094248) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
