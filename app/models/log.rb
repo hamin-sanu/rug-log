ae5c7fe14bb77ef7769011cdf5b3118961ed5575
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-03-02 20:47:46 +0900

    バリデーションを記述
    新規投稿のバリデーションを設定

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,7 +5,15 @@ class Log < ApplicationRecord
   with_options presence: true do
     validates :user_id
     validates :category_id
+    validates :body
+    validates :matchData
+    validates :how, acceptance: true
+    validates :team_a, length: { maximum: 8 }
+    validates :team_b, length: { maximum: 8 }
   end
+
+  validates :score_a, format: { with: /\A[0-9]+\z/ }
+  validates :score_b, format: { with: /\A[0-9]+\z/ }
 
   default_scope -> { order(created_at: :desc) }
 end
