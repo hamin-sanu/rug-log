6760be3cd9079795413200bd81ab19a59b20817d
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2021-03-05 20:33:11 +0900

    JS導入でアコーディオン
    トグルで開閉できるようになった

--- app/assets/javaScripts/modules/log.js
+++ app/assets/javaScripts/modules/log.js
@@ -1,9 +1,13 @@
 $(function(){
-  $('.__acBtnUp').click(function(){
-    console.log('Up')
+  $('.__acBtnDown').click(function(){ 
+    $(this).parents('.__block').find('.body').toggle(400);
+    $(this).parents('.__block').find('.__acBtnUp').toggle(400);
+    $(this).parents('.__block').find('.__acBtnDown').toggle('slow');
   });
-  
-  $('.__acBtnDown').click(function(){
-    console.log('Down')
+
+  $('.__acBtnUp').click(function(){
+    $(this).parents('.__block').find('.body').toggle(400);
+    $(this).parents('.__block').find('.__acBtnDown').toggle(400);
+    $(this).parents('.__block').find('.__acBtnUp').toggle('slow');
   });
 });
